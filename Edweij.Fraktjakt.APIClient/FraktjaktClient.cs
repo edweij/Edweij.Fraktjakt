2adbf2007dd50f7df70ee12f562ad23535cec0ef
Edvin Weijmers <edvin@eddy.se>
2024-02-21 18:08:14 +0100

    Added interface for client and Helper extensions

--- Edweij.Fraktjakt.APIClient/FraktjaktClient.cs
+++ Edweij.Fraktjakt.APIClient/FraktjaktClient.cs
@@ -1,126 +1,141 @@
 ﻿using Edweij.Fraktjakt.APIClient.RequestModels;
 using Edweij.Fraktjakt.APIClient.ResponseModels;
 using Edweij.Fraktjakt.APIClient.Structs;
+using Microsoft.Extensions.DependencyInjection;
 using System.Web;
 
-namespace Edweij.Fraktjakt.APIClient
+namespace Edweij.Fraktjakt.APIClient;
+
+public class FraktjaktClient : IFraktjaktClient, IDisposable
 {
-    public class FraktjaktClient : IDisposable
+    private readonly HttpClient _httpClient;
+    private readonly bool _useMD5Checksum = true;
+
+    public Sender Sender { get; init; }
+
+
+    public FraktjaktClient(int id, string key, bool useMD5Checksum = true) {
+        if (id <= 0) throw new ArgumentException(nameof(id));
+        if (string.IsNullOrWhiteSpace(key)) throw new ArgumentException(nameof(key));
+
+        _httpClient = new HttpClient();
+        _httpClient.BaseAddress = new Uri("https://api.fraktjakt.se");
+        Sender = new Sender(id, key);
+        _useMD5Checksum = useMD5Checksum;
+    }
+
+    public FraktjaktClient(Sender sender, bool useMD5Checksum = true)
     {
-        private readonly HttpClient _httpClient;
-        private readonly bool _useMD5Checksum = true;
+        if (sender == null) throw new ArgumentNullException(nameof(sender));
+        if (!sender.IsValid) throw new ArgumentException("Sender is not valid");
 
-        public Sender Sender { get; init; }
+        _httpClient = new HttpClient();
+        _httpClient.BaseAddress = new Uri("https://api.fraktjakt.se");
+        Sender = sender;
+        _useMD5Checksum = useMD5Checksum;
+    }
 
+    public async Task<Response> Trace(int shipmentId, SwedishOrEnglish lang)
+    {
+        var url = $"/trace/xml_trace?consignor_id={Sender.Id}&consignor_key={Sender.Key}&shipment_id={shipmentId}&locale={lang}";
+        var response = await _httpClient.GetAsync(url);
+        return await TraceResponse.FromHttpResponse(response);
+    }
 
-        public FraktjaktClient(int id, string key, bool useMD5Checksum = true) {
-            if (id <= 0) throw new ArgumentException(nameof(id));
-            if (string.IsNullOrWhiteSpace(key)) throw new ArgumentException(nameof(key));
+    public async Task<Response> ShippingDocuments(int shipmentId, SwedishOrEnglish lang)
+    {
+        var url = $"/shipping_documents/xml_get?consignor_id={Sender.Id}&consignor_key={Sender.Key}&shipment_id={shipmentId}&locale={lang}";
+        var response = await _httpClient.GetAsync(url);
+        return await ShippingDocumentsResponse.FromHttpResponse(response);
+    }
 
-            _httpClient = new HttpClient();
-            _httpClient.BaseAddress = new Uri("https://api.fraktjakt.se");
-            Sender = new Sender(id, key);
-            _useMD5Checksum = useMD5Checksum;
-        }
+    public async Task<Response> Query(ShipmentQuery shipment)
+    {
+        if (shipment == null) throw new ArgumentNullException(nameof(shipment));            
+        if (!shipment.IsValid) throw new ArgumentException("Shipment is not valid");
+        if (shipment.Sender != Sender) throw new ArgumentException("Sender in shipment is different from the clients sender");
 
-        public FraktjaktClient(Sender sender, bool useMD5Checksum = true)
+        var xml = @"<?xml version=""1.0"" encoding=""UTF-8""?>" + shipment.ToXml();
+        var url = $"/fraktjakt/query_xml?xml={UrlEncode(xml)}";
+        if (_useMD5Checksum)
         {
-            if (sender == null) throw new ArgumentNullException(nameof(sender));
-            if (!sender.IsValid) throw new ArgumentException("Sender is not valid");
-
-            _httpClient = new HttpClient();
-            _httpClient.BaseAddress = new Uri("https://api.fraktjakt.se");
-            Sender = sender;
-            _useMD5Checksum = useMD5Checksum;
+            url += $"&md5_checksum={MD5(xml)}";
         }
+        var response = await _httpClient.GetAsync(url);
+        return await ShipmentResponse.FromHttpResponse(response);
+    }
 
-        public async Task<Response> Trace(int shipmentId, SwedishOrEnglish lang)
+    public async Task<Response> Order(Order order)
+    {
+        if (order == null) throw new ArgumentNullException(nameof(order));
+        if (!order.IsValid) throw new ArgumentException("Order is not valid");
+        if (order.Sender != Sender) throw new ArgumentException("Sender in order is different from the clients sender");
+
+        var xml = @"<?xml version=""1.0"" encoding=""UTF-8""?>" + order.ToXml();
+        var url = $"/orders/order_xml?xml={UrlEncode(xml)}";
+        if (_useMD5Checksum)
         {
-            var url = $"/trace/xml_trace?consignor_id={Sender.Id}&consignor_key={Sender.Key}&shipment_id={shipmentId}&locale={lang}";
-            var response = await _httpClient.GetAsync(url);
-            return await TraceResponse.FromHttpResponse(response);
+            url += $"&md5_checksum={MD5(xml)}";
         }
+        var response = await _httpClient.GetAsync(url);
+        return await OrderResponse.FromHttpResponse(response);
+    }
 
-        public async Task<Response> ShippingDocuments(int shipmentId, SwedishOrEnglish lang)
+    public async Task<Response> CreateShipment(CreateShipment createShipment)
+    {
+        if (createShipment == null) throw new ArgumentNullException(nameof(createShipment));
+        if (!createShipment.IsValid) throw new ArgumentException("createShipment is not valid");
+        if (createShipment.Sender != Sender) throw new ArgumentException("Sender in createShipment is different from the clients sender");
+
+        var xml = @"<?xml version=""1.0"" encoding=""UTF-8""?>" + createShipment.ToXml();
+        var url = $"/shipments/shipment_xml?xml={UrlEncode(xml)}";
+        if (_useMD5Checksum)
         {
-            var url = $"/shipping_documents/xml_get?consignor_id={Sender.Id}&consignor_key={Sender.Key}&shipment_id={shipmentId}&locale={lang}";
-            var response = await _httpClient.GetAsync(url);
-            return await ShippingDocumentsResponse.FromHttpResponse(response);
+            url += $"&md5_checksum={MD5(xml)}";
         }
+        var response = await _httpClient.GetAsync(url);
+        return await CreateShipmentResponse.FromHttpResponse(response);
+    }
 
-        public async Task<Response> Query(ShipmentQuery shipment)
+    public async Task<Response> GetServicePoints(string url)
+    {
+        if (string.IsNullOrEmpty(url)) throw new ArgumentNullException(nameof(url));
+        var response = await _httpClient.GetAsync(url);
+        return await AgentListResponse.FromHttpResponse(response);
+    }
+
+    public static string UrlEncode(string input)
+    {
+        return HttpUtility.UrlEncode(input);
+    }
+
+    public static string MD5(string input)
+    {
+        using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
         {
-            if (shipment == null) throw new ArgumentNullException(nameof(shipment));            
-            if (!shipment.IsValid) throw new ArgumentException("Shipment is not valid");
-            if (shipment.Sender != Sender) throw new ArgumentException("Sender in shipment is different from the clients sender");
-
-            var xml = @"<?xml version=""1.0"" encoding=""UTF-8""?>" + shipment.ToXml();
-            var url = $"/fraktjakt/query_xml?xml={UrlEncode(xml)}";
-            if (_useMD5Checksum)
-            {
-                url += $"&md5_checksum={MD5(xml)}";
-            }
-            var response = await _httpClient.GetAsync(url);
-            return await ShipmentResponse.FromHttpResponse(response);
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hashBytes = md5.ComputeHash(inputBytes);
+            return Convert.ToHexString(hashBytes);
         }
+    }
 
-        public async Task<Response> Order(Order order)
-        {
-            if (order == null) throw new ArgumentNullException(nameof(order));
-            if (!order.IsValid) throw new ArgumentException("Order is not valid");
-            if (order.Sender != Sender) throw new ArgumentException("Sender in order is different from the clients sender");
+    public void Dispose()
+    {
+        _httpClient.Dispose();
+    }
+}
 
-            var xml = @"<?xml version=""1.0"" encoding=""UTF-8""?>" + order.ToXml();
-            var url = $"/orders/order_xml?xml={UrlEncode(xml)}";
-            if (_useMD5Checksum)
-            {
-                url += $"&md5_checksum={MD5(xml)}";
-            }
-            var response = await _httpClient.GetAsync(url);
-            return await OrderResponse.FromHttpResponse(response);
-        }
+public static class FraktjaktClientExtensions
+{
+    public static IServiceCollection AddFraktjaktClient(this IServiceCollection services, int senderId, string senderKey, bool useMD5Checksum = true)
+    {
+        services.AddSingleton<IFraktjaktClient>(new FraktjaktClient(senderId, senderKey, useMD5Checksum));
+        return services;
+    }
 
-        public async Task<Response> CreateShipment(CreateShipment createShipment)
-        {
-            if (createShipment == null) throw new ArgumentNullException(nameof(createShipment));
-            if (!createShipment.IsValid) throw new ArgumentException("createShipment is not valid");
-            if (createShipment.Sender != Sender) throw new ArgumentException("Sender in createShipment is different from the clients sender");
-
-            var xml = @"<?xml version=""1.0"" encoding=""UTF-8""?>" + createShipment.ToXml();
-            var url = $"/shipments/shipment_xml?xml={UrlEncode(xml)}";
-            if (_useMD5Checksum)
-            {
-                url += $"&md5_checksum={MD5(xml)}";
-            }
-            var response = await _httpClient.GetAsync(url);
-            return await CreateShipmentResponse.FromHttpResponse(response);
-        }
-
-        public async Task<Response> GetServicePoints(string url)
-        {
-            if (string.IsNullOrEmpty(url)) throw new ArgumentNullException(nameof(url));
-            var response = await _httpClient.GetAsync(url);
-            return await AgentListResponse.FromHttpResponse(response);
-        }
-
-        public static string UrlEncode(string input)
-        {
-            return HttpUtility.UrlEncode(input);
-        }
-
-        public static string MD5(string input)
-        {
-            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
-            {
-                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
-                byte[] hashBytes = md5.ComputeHash(inputBytes);
-                return Convert.ToHexString(hashBytes);
-            }
-        }
-
-        public void Dispose()
-        {
-            _httpClient.Dispose();
-        }
+    public static IServiceCollection AddFraktjaktClient(this IServiceCollection services, Sender sender, bool useMD5Checksum = true)
+    {
+        services.AddSingleton<IFraktjaktClient>(new FraktjaktClient(sender, useMD5Checksum));
+        return services;
     }
 }
---
+++ Edweij.Fraktjakt.APIClient/IFraktjaktClient.cs
@@ -0,0 +1,15 @@
+﻿using Edweij.Fraktjakt.APIClient.RequestModels;
+using Edweij.Fraktjakt.APIClient.ResponseModels;
+using Edweij.Fraktjakt.APIClient.Structs;
+
+namespace Edweij.Fraktjakt.APIClient;
+
+public interface IFraktjaktClient
+{
+    public Task<Response> Trace(int shipmentId, SwedishOrEnglish lang);
+    public Task<Response> ShippingDocuments(int shipmentId, SwedishOrEnglish lang);
+    public Task<Response> Query(ShipmentQuery shipment);
+    public Task<Response> Order(Order order);
+    public Task<Response> CreateShipment(CreateShipment createShipment);
+    public Task<Response> GetServicePoints(string url);
+}
